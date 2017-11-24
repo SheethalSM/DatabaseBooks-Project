e6e797bdac15767607bda3b2a7cb4f3d56e6c97f
SheethalSM <sheethalsmathew@gmail.com>
2017-11-23 23:46:44 -0800

    I think first two queries are working
    I dont know how to check if selection is working

--- Driver.java
+++ Driver.java
@@ -1,18 +1,73 @@
 package jdbcdemo;
 import java.sql.*;
+import java.util.Properties;
 import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Driver {
 	public static void main(String[] args) throws Exception {
-		ConnectionToMySQL();
 		Driver dObject = new Driver();
-		//dObject.createTableAuthors();
-		//dObject.createTableAuthorISBN();
-		//dObject.createTablePublishers();
-		//dObject.createTitles();
+		ResultSet myRs = null;
+		ResultSet myRspub = null;
+		try{
+			ConnectionToMySQL();
+			
+			//dObject.createTableAuthors();
+			//dObject.createTableAuthorISBN();
+			//dObject.createTablePublishers();
+			//dObject.createTitles();
+			//dObject.InsertDataAuthors("Jesus1", "First", 909090); //sample manual insertion of data
+			stMt = connect.createStatement();
+			myRs =stMt.executeQuery("select publisherName, publisherID from publishers");
+			myRs = stMt.executeQuery("select lastName, firstName from authors WHERE lastName LIKE 'alpha%'");
+						
+			//line 23-34 shows if selecting query from author works
+			System.out.println(myRs.findColumn("firstName") + " is the column number of firstName");
+			ResultSetMetaData rsMetaData = myRs.getMetaData();
+			
+			int columnCount = rsMetaData.getColumnCount();
+			System.out.println("Column count in authors table: " +columnCount+ "\n");
+			for(int col=1; col<=columnCount; col++)
+			{
+				System.out.println("Column name: " + rsMetaData.getColumnName(col));
+				 System.out.println("column label print as string: "+ rsMetaData.getColumnLabel(col)); ;
+				System.out.println("Column type: " + rsMetaData.getColumnTypeName(col));
+
+			}   
+			
+			//dObject.populateTable();
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		finally{
+			if(dObject!=null){
+				try{
+					dObject.close();
+				}catch (Exception e){}
+			}
+		}
+		
 	}
+	
+	/**
+	 * Not necessary but useful in closing the connection to database to avoid security breach
+	 */
+	public void close(){
+	   if(connect!= null){
+	    try {
+	        connect.close();
+	        } catch (SQLException e) {
+	             e.printStackTrace();
+	        }
+	  }
+	}
+	
+	/**
+	 * Initiates Connection
+	 * ignore line 82-97, unless this type of connection is not working for you
+	 */
 	public static void getConnection() {//throws Exception {
 		//Connection myConn = null;
 		try{
@@ -125,21 +180,33 @@ public class Driver {
 			e.printStackTrace();
 		}
 	}*/
+	
+	/**
+	 * Connection reference: http://www.razorsql.com/articles/mysql_ssl_jdbc.html
+	 * This is useful incase there is SSL connectivity problems
+	 */
 	public static void ConnectionToMySQL() {
 		getConnection();
-		String host = "jdbc:mysql://127.0.0.1:3306/books";
-		String password = "sheethal1";
+		String host = "jdbc:mysql://127.0.0.1:3306/books"+
+				"?verifyServerCertificate=false"+
+				"&useSSL=true"+
+				"&requireSSL = true";
+		
 		String username = "root";
+		String password = "sheethal1";
+		
 		try{
-			connect = DriverManager.getConnection(host,username,password);
-			System.out.println("works Connection.sql connect");
+			connect = DriverManager.getConnection(host,username, password);
+			System.out.println("Connection.sql connect works");
 		} catch (SQLException e) {
 			//System.out.println(e);
 			e.printStackTrace();
 			
 		}
+		
 	}
 	/**
+	 * This method generates random words that could be used in several columns like first and last names, title etc
 	 * Reference of random word generator
 	 * https://stackoverflow.com/questions/4951997/generating-random-words-in-java
 	 * 
@@ -160,8 +227,33 @@ public class Driver {
 		return words;
 		
 	}
-	Statement stMt;
-	public void populateTable()
+
+	/**
+	 * This methods lets you manually insert Data into a table
+	 * @param firstName
+	 * @param lastName
+	 * @param authorID
+	 * Reference: Techies Coding Arena youtube channel.. Tutorial 10
+	 */
+	private void InsertDataAuthors(String firstName, String lastName, int authorID) {
+		PreparedStatement pst = null;
+		try{
+			pst =connect.prepareStatement("INSERT INTO authors (firstName, lastName, authorID) VALUES (?,?,?)");
+			pst.setString(1, firstName);
+			pst.setInt(3, authorID);
+			pst.setString(2, lastName);
+			pst.executeUpdate();
+			System.out.println("Inserted to authors table successfully");
+		}catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	static Statement stMt;
+	/**
+	 * This method should idaelly populate table,,, but I am not sure why it is not populating 
+	 */
+	/*public void populateTable()
 	{
 		try {
 			String[] firstName = wordGenerator(20);
@@ -172,21 +264,20 @@ public class Driver {
 			for(int i=1; i<19; i++)
 			{
 				//Lecture 19-21, slide#12
-				String inAuthor = "INSERT INTO BOOKS.authors(authorID, firstName, lastName) VALUES (" + i+", ' " + firstName[i]+"', '" +lastName[i]+ "');";
+				String inAuthor = "INSERT INTO books.authors(authorID, firstName, lastName) VALUES (" + i+", ' " + firstName[i]+"', '" +lastName[i]+ "');";
 				stMt.execute(inAuthor);
-				String inPublisher = "INSERT INTO BOOKS.publishers (publisherID, publisherName) VALUES (" + i +", '" + publisherName[i] + "');";
+				String inPublisher = "INSERT INTO books.publishers (publisherID, publisherName) VALUES (" + i +", '" + publisherName[i] + "');";
 				stMt.execute(inPublisher);
-				String inTitles = "INSERT INTO BOOKS.titles (isbn, title, publisherID, copyright) VALUES ('isbn" + i + "', '" + title[i] + "', " +i +" , "+ (2000+i)+");";	
+				String inTitles = "INSERT INTO books.titles (isbn, title, publisherID, copyright) VALUES ('isbn" + i + "', '" + title[i] + "', " +i +" , "+ (2000+i)+");";	
 				stMt.execute(inTitles);
-				String inAuthorISBN = "INSERT INTO BOOKS.authorISBN (authorID, isbn VALUES (" + i +", 'isbn" + i + "');";
+				String inAuthorISBN = "INSERT INTO books.authorISBN (authorID, isbn VALUES (" + i +", 'isbn" + i + "');";
 				stMt.execute(inAuthorISBN);
 				}
 		}
 		 catch (Exception e) {
-	            e.printStackTrace();
+	            System.out.println(e +" 264");;
 		 }
 	
-	
-	}
+	}*/
 	
 }
